ee5e00173d7df6782d8daec9a88054dd3383d0f9
Essam Fakher <50755435+essam5@users.noreply.github.com>
2021-02-28 19:40:22 +0200

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = "Secret Key"
  
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/crud'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
